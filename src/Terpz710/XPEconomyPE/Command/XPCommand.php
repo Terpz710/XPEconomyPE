8f2134e75428377e56f1a16c4d6a1a3b432e8147
BlueGamesNetwork <142659461+Terpz710@users.noreply.github.com>
2023-12-23 16:55:48 -0800

    BlueGamesNetwork LLC

---
+++ src/Terpz710/XPEconomyPE/Command/XPCommand.php
@@ -0,0 +1,198 @@
+<?php
+
+namespace Terpz710\XPEconomy\command;
+
+use pocketmine\command\Command;
+use pocketmine\command\CommandSender;
+use pocketmine\player\Player;
+
+use Terpz710\XPEconomy\Experience;
+
+class ExpCommand extends Command {
+
+    /** @var Experience */
+    private $plugin;
+
+    public function __construct(Experience $plugin) {
+        parent::__construct("exp", "XP Economy command", "/exp [see|pay|myxp|topxp|removexp|setxp|addxp]", ["xp"]);
+
+        $this->plugin = $plugin;
+    }
+
+    public function execute(CommandSender $sender, string $commandLabel, array $args): bool {
+        if (!$sender instanceof Player) {
+            $sender->sendMessage("This command can only be used in-game.");
+            return true;
+        }
+
+        if (count($args) === 0) {
+            $sender->sendMessage("Usage: /exp [see|pay|myxp|topxp|removexp|setxp|addxp]");
+            return false;
+        }
+
+        $subCommand = strtolower(array_shift($args));
+
+        switch ($subCommand) {
+            case "see":
+                $this->seeExp($sender, $args);
+                break;
+
+            case "pay":
+                $this->payExp($sender, $args);
+                break;
+
+            case "myxp":
+                $this->checkExp($sender);
+                break;
+
+            case "topxp":
+                $this->topExp($sender);
+                break;
+
+            case "removexp":
+                if (!$sender->hasPermission("xpeconomype.cmd.removeexp")) {
+                        return true;
+                    }
+                $this->removeExp($sender, $args);
+                break;
+
+            case "setxp":
+                if (!$sender->hasPermission("xpeconomype.cmd.setexp")) {
+                        return true;
+                    }
+                $this->setExp($sender, $args);
+                break;
+
+            case "addxp":
+                if (!$sender->hasPermission("xpeconomype.cmd.addexp")) {
+                        return true;
+                    }
+                $this->addExp($sender, $args);
+                break;
+
+            default:
+                $sender->sendMessage("Usage: /exp [see|pay|myxp|topxp|removexp|setxp|addxp]");
+                break;
+        }
+
+        return false;
+    }
+
+    private function seeExp(Player $sender, array $args): void {
+        if (count($args) === 0) {
+            $sender->sendMessage("Usage: /exp see <player>");
+            return;
+        }
+
+        $targetPlayerName = array_shift($args);
+        $exp = $this->plugin->getPlayerExp($targetPlayerName);
+        
+        if ($exp === null) {
+            $sender->sendMessage("Player not found or has no EXP.");
+        } else {
+            $sender->sendMessage("{$targetPlayerName}'s EXP balance: " . $exp);
+        }
+    }
+
+    private function payExp(Player $sender, array $args): void {
+        if (count($args) < 2) {
+            $sender->sendMessage("Usage: /exp pay <player> <amount>");
+            return;
+        }
+
+        $targetPlayerName = array_shift($args);
+        $amount = intval(array_shift($args));
+
+        if ($amount <= 0) {
+            $sender->sendMessage("Amount must be a positive integer.");
+            return;
+        }
+
+        $success = $this->plugin->payExp($sender, $targetPlayerName, $amount);
+
+        if ($success) {
+            $sender->sendMessage("Paid {$amount} EXP to {$targetPlayerName}.");
+        } else {
+            $sender->sendMessage("Failed to pay EXP. Check if the target player exists and has enough EXP.");
+        }
+    }
+
+    private function topExp(Player $sender): void {
+        $topPlayers = $this->plugin->getTopPlayers();
+
+        if (empty($topPlayers)) {
+            $sender->sendMessage("No players with EXP found.");
+            return;
+        }
+
+        $sender->sendMessage("Top players by EXP:");
+
+        foreach ($topPlayers as $position => $playerData) {
+            $sender->sendMessage("{$position}. {$playerData['name']} - {$playerData['exp']} EXP");
+        }
+    }
+
+    private function removeExp(Player $sender, array $args): void {
+        if (count($args) < 2) {
+            $sender->sendMessage("Usage: /exp removexp <player> <amount>");
+            return;
+        }
+
+        $targetPlayerName = array_shift($args);
+        $amount = intval(array_shift($args));
+
+        if ($amount <= 0) {
+            $sender->sendMessage("Amount must be a positive integer.");
+            return;
+        }
+
+        $success = $this->plugin->removeExp($targetPlayerName, $amount);
+
+        if ($success) {
+            $sender->sendMessage("Removed {$amount} EXP from {$targetPlayerName}.");
+        } else {
+            $sender->sendMessage("Failed to remove EXP. Check if the target player exists and has enough EXP.");
+        }
+    }
+
+    private function setExp(Player $sender, array $args): void {
+        if (count($args) < 2) {
+            $sender->sendMessage("Usage: /exp setxp <player> <level>");
+            return;
+        }
+
+        $targetPlayerName = array_shift($args);
+        $level = intval(array_shift($args));
+
+        $success = $this->plugin->setPlayerExp($targetPlayerName, $level);
+
+        if ($success) {
+            $sender->sendMessage("Set {$targetPlayerName}'s experience level to {$level}.");
+        } else {
+            $sender->sendMessage("Failed to set experience level. Check if the target player exists.");
+        }
+    }
+
+    private function addExp(Player $sender, array $args): void {
+        if (count($args) < 2) {
+            $sender->sendMessage("Usage: /exp addxp <player> <amount>");
+            return;
+        }
+
+        $targetPlayerName = array_shift($args);
+        $amount = intval(array_shift($args));
+
+        if ($amount <= 0) {
+            $sender->sendMessage("Amount must be a positive integer.");
+            return;
+        }
+
+        $success = $this->plugin->addExp($targetPlayerName, $amount);
+
+        if ($success) {
+            $sender->sendMessage("Added {$amount} EXP to {$targetPlayerName}.");
+        } else {
+            $sender->sendMessage("Failed to add EXP. Check if the target player exists.");
+        }
+    }
+}
---
+++ src/Terpz710/XPEconomyPE/Experience.php
@@ -0,0 +1,139 @@
+<?php
+
+namespace Terpz710\XPEconomy;
+
+use pocketmine\player\Player;
+use pocketmine\utils\Config;
+use pocketmine\event\Listener;
+use pocketmine\event\player\PlayerJoinEvent;
+use pocketmine\event\player\PlayerQuitEvent;
+use pocketmine\plugin\PluginBase;
+
+use Terpz710\XPEconomy\Command\XPCommand;
+
+class Experience extends PluginBase implements Listener {
+
+    /** @var Config */
+    private $playerData;
+
+    public function onEnable(): void {
+        $this->getServer()->getPluginManager()->registerEvents($this, $this);
+
+        $this->playerData = new Config($this->getDataFolder() . "playerData.json", Config::JSON);
+        
+        $this->getServer()->getCommandMap()->register("exp", new ExpCommand($this));
+    }
+
+    public function onDisable(): void {
+        $this->playerData->save();
+    }
+
+    public function onJoin(PlayerJoinEvent $event) {
+        $player = $event->getPlayer();
+        $playerName = $player->getName();
+        if ($this->existPlayer($playerName)) {
+            $exp = $this->getPlayerExp($playerName);
+            $this->setPlayerExpLevel($player, $exp);
+            $this->getLogger()->info($playerName . " has " . $exp . " EXP!"); // Manual security check!
+        } else {
+            $this->createPlayer($playerName);
+            $this->getLogger()->info("Created a new profile for " . $playerName . "!"); // Very usless but cool.
+        }
+    }
+
+    public function onQuit(PlayerQuitEvent $event) {
+        $player = $event->getPlayer();
+        $playerName = $player->getName();
+        $exp = $this->getPlayerExpLevel($player);
+        $this->setPlayerExp($playerName, $exp);
+    }
+
+    public function getPlayerName(Player $player): string {
+        return $player->getName();
+    }
+
+    public function existPlayer(string $playerName): bool {
+        return $this->playerData->exists($playerName);
+    }
+
+    public function createPlayer(string $playerName): void {
+        $this->playerData->set($playerName, ["exp" => 0]);
+        $this->playerData->save();
+    }
+
+    public function addExp(Player $player, int $amount): bool {
+        $playerName = $this->getPlayerName($player);
+        $currentExp = $this->getPlayerExp($playerName);
+        $newExp = $currentExp + $amount;
+        $this->setExp($playerName, $newExp);
+        $this->setPlayerExpLevel($player, $newExp);
+        return true;
+    }
+
+    public function removeExp(string $playerName, int $amount): bool {
+        $currentExp = $this->getPlayerExp($playerName);
+        $newExp = max(0, $currentExp - $amount);
+        $this->setExp($playerName, $newExp);
+        return true;
+    }
+
+    public function setExp(string $playerName, int $amount): void {
+        $this->playerData->setNested($playerName . ".exp", $amount);
+    }
+
+    public function getPlayerExp(string $playerName): int {
+        return $this->playerData->getNested($playerName . ".exp", 0);
+    }
+
+    public function setPlayerExpLevel(Player $player, int $level): void {
+        $player->setExperience($this->calculateExpFromLevel($level));
+    }
+
+    public function getPlayerExpLevel(Player $player): int {
+        return $this->calculateLevelFromExp($player->getExperience());
+    }
+
+    private function calculateExpFromLevel(int $level): float {
+        return (2 * $level ** 3) + (5 * $level ** 2) + (100 * $level);
+    }
+
+    private function calculateLevelFromExp(float $exp): int {
+        $level = 0;
+        $requiredExp = 0;
+
+        while ($exp >= $requiredExp) {
+            $level++;
+            $requiredExp += $this->calculateExpFromLevel($level);
+        }
+
+        return max(0, $level - 1);
+    }
+
+    public function getTopPlayers(int $limit = 5): array {
+    $players = [];
+
+    foreach ($this->playerData->getAll() as $playerName => $data) {
+        $players[$playerName] = $data['exp'];
+    }
+
+    arsort($players);
+
+    $topPlayers = [];
+    $position = 1;
+
+    foreach ($players as $playerName => $exp) {
+        $topPlayers[$position] = [
+            'name' => $playerName,
+            'exp' => $exp,
+        ];
+
+        $position++;
+
+        if ($position > $limit) {
+            break;
+            }
+        }
+
+    return $topPlayers;
+    }
+}// fuck niggas
